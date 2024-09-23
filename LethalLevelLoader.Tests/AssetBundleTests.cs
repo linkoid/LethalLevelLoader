6d53cec890b0a6efa8512dd32686b37efb1f449b
linkoid <36754150+linkoid@users.noreply.github.com>
2024-09-23 13:54:15 -0400

    Updated tests to use an actual bundle

---
+++ LethalLevelLoader.Tests/AssetBundleTests.cs
@@ -0,0 +1,94 @@
+﻿using BepInEx.Logging;
+using FluentAssertions;
+using SmiteUnit.Framework;
+using SmiteUnit.Unity;
+using System.Collections;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LethalLevelLoader.Tests
+{
+    /// <summary>
+    /// Ensure that Lethal Level Loader will properly load asset bundles.
+    /// </summary>
+    [SmiteProcess("%GAME_EXE_PATH%", "-logfile -")]
+    public class AssetBundleTests : MonoBehaviour
+    {
+        protected static AssetBundleTests Instance { get; private set; } = null!;
+        protected static ManualLogSource Logger { get; private set; } = null!;
+
+        protected static readonly string BundlesDirectory =
+            Path.Combine(
+                Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location!)!.FullName,
+                "TestBundles"
+            );
+
+        [SmiteSetUp]
+        public static void SetUp()
+        {
+            Instance = new GameObject($"{typeof(AssetBundleTests).Name}").AddComponent<AssetBundleTests>();
+            GameObject.DontDestroyOnLoad(Instance.gameObject);
+            Instance.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+            Logger = BepInEx.Logging.Logger.CreateLogSource(typeof(AssetBundleTests).Assembly.GetName().Name);
+        }
+
+        [SmiteTest]
+        public static async Task LoadExtendedModBundle()
+        {
+            // Load an asset bundle with an extended mod
+            string fileName = "starlancermoons.testbundle";
+            string bundleFile = Path.Combine(BundlesDirectory, fileName); 
+
+            await Instance.StartCoroutineTask(AssetBundleLoader.Instance.LoadBundle(bundleFile, fileName));
+
+            // Assert that the bundle was loaded
+            AssetBundleLoader.assetBundles.TryGetValue(fileName, out var assetBundle);
+            assetBundle.Should().NotBeNull();
+
+            // Log the keys of all loaded extended mods
+            Logger.LogDebug($"Extended Mods Dictionary Keys ({AssetBundleLoader.obtainedExtendedModsDictionary.Count}): " +
+                $"{string.Join(", ", AssetBundleLoader.obtainedExtendedModsDictionary.Keys)}");
+
+            // Assert that the extended mod was loaded
+            AssetBundleLoader.obtainedExtendedModsDictionary.TryGetValue("AudioKnight", out var extendedMod);
+            extendedMod.Should().NotBeNull();
+        }
+
+        [SmiteTest]
+        public static async Task LoadStreamedSceneAssetBundle()
+        {
+            // Load an asset bundle with scenes
+            string fileName = "starlancermoonsscenes.testbundle";
+            string bundleFile = Path.Combine(BundlesDirectory, fileName);
+
+            await Instance.StartCoroutineTask(AssetBundleLoader.Instance.LoadBundle(bundleFile, fileName));
+
+            // Assert that the bundle was loaded
+            AssetBundleLoader.assetBundles.TryGetValue(fileName, out var assetBundle);
+            assetBundle.Should().NotBeNull();
+        }
+
+        [SmiteTest]
+        public static async Task LoadExtendedLevels()
+        {
+            // First, load the scenes
+            await LoadStreamedSceneAssetBundle();
+
+            // Second, load the extended mod
+            await LoadExtendedModBundle();
+
+            AssetBundleLoader.obtainedExtendedModsDictionary.TryGetValue("AudioKnight", out var extendedMod);
+            extendedMod.Should().NotBeNull();
+
+            // Assert that all the extended levels were loaded
+            extendedMod!.ExtendedLevels.Count.Should().Be(3);
+            foreach (var level in extendedMod.ExtendedLevels)
+            {
+                level.SelectableLevel.Should().NotBeNull();
+                level.SelectableLevel.sceneName.Should().NotBeNullOrWhiteSpace();
+            }
+        }
+    }
+}
---
+++ LethalLevelLoader.Tests/TestBundles/NOTICE.md
@@ -0,0 +1,5 @@
+﻿The files `starlancermoons.testbundle` and `starlancermoonsscenes.testbundle` are from the mod 
+[StarlancerMoons](https://thunderstore.io/c/lethal-company/p/AudioKnight/StarlancerMoons/) v1.7.1,
+copyright AudioKnight, licensed under [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/).
+
+No modifications were made to any files other than changing the file extension.
